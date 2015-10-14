d0c9ef83adfbd69b9cb0825c843059b8e12eb9b0
Kasey Delk <delkk@student.swosu.edu>
2015-10-13 22:25:09 -0500

    trimmed down code

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,19 +1,5 @@
-/**
- * 
- */
-
-/**
- * @author Kasey
- *
- */
 public class ForLoops {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }
