759ce169c399a2c32dc0e925120ee9b3b85e2b4e
Kasey Delk <delkk@student.swosu.edu>
2015-10-13 22:32:38 -0500

    added working code from wiki

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,5 +1,11 @@
 public class ForLoops {
 	public static void main(String[] args) {
 		
+		for (int i = 0; i < 100; i++)
+		{
+		    System.out.println(i);
+		    System.out.println(' ');
+		}
+		System.out.println();
 	}
 }
